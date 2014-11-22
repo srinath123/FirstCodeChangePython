c989855d7aa420df57f024f0998f5adf6df74643
srinath123 <srinath.venigandla@gmail.com>
2014-11-22 19:49:13 +0530

    rearranging files

---
+++ scikit testing/testingSVM.py
@@ -0,0 +1,6 @@
+from sklearn import datasets
+iris = datasets.load_iris()
+from sklearn.svm import SVC
+s = SVC()
+y_pred=s.fit(iris.data,iris.target).predict(iris.data)
+print("Number of mislabeled points out of a total %d points : %d"  % (iris.data.shape[0],(iris.target != y_pred).sum()))
